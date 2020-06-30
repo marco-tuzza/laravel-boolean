0edd3db08cd3cd8f73ae6e1e6fe1efa66a7e1e10
Marco Tuzza <marcotuzza@hotmail.it>
2020-06-30 16:02:41 +0200

    Prima versione header

---
+++ MyProject/resources/views/layouts/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+   @extends('partials.header') 
+</body>
+</html>
---
+++ MyProject/resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="header-container">
+        <div class="header-logo">
+            <img src="https://www.boolean.careers/images/common/logo.png" alt="Logo di Boolean Careers">
+        </div>
+        <div class="header-nav">
+            <ul>
+                <li class="active">Home</li>
+                <li>Corso</li>
+                <li>Dopo il corso</li>
+                <li>Lezione gratuita</li>
+                <li>Assumi i nostri studenti</li>
+            </ul>
+            <button>Candidati ora</button>
+        </div>
+    </div>
+</header>
