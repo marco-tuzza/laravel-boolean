1cd6e533c0f9a1fc5bede2222da3dfe5797a7bb9
Marco Tuzza <marcotuzza@hotmail.it>
2020-07-01 15:25:21 +0200

    Struttura domanda risposta fatta

--- MyProject/resources/views/faq.blade.php
+++ MyProject/resources/views/faq.blade.php
@@ -6,6 +6,31 @@
             <h2>Domande frequenti</h2>
             <h3>Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? <a href="">Contattaci</a> e saremo felici di darti una mano.</h3>
         </div>
+        <div class="faq-content">
+            <div class="faq-content-left">
+                <h3>Prima del corso</h3>
+                <div class="faq-element">
+                    <div class="faq-question">
+                        <h4>Chi può fare il corso Boolean?</h4>
+                    </div>
+                    <div class="faq-answer">
+                        <p>Chiunque abbia una forte curiosità verso il mondo della programmazione e voglia di imparare a fare il mestiere di sviluppatore.
+                        <p>Età e curriculum non sono una discriminante: infatti, i nostri studenti hanno dai 18 ai 45 anni, sono giovani ragazzi senza alcuna esperienza lavorativa, ma anche affermati professionisti che vogliono cambiare settore.Se non hai mai programmato o se hai già seguito qualche corso ma senti di non aver approfondito abbastanza per diventare programmatore, Boolean è il corso che fa per te.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="faq-content-right">
+                <h3>Dopo il corso</h3>   
+                <div class="faq-element">
+                    <div class="faq-question">
+                        <h4>Ho la certezza di essere assunto?</h4>
+                    </div>
+                    <div class="faq-answer">
+                        <p>Dipende tutto da te! Come dimostrano numerosi studi di settore, lo sviluppatore web è uno dei mestieri più ricercati in Italia e Boolean fornisce tutte le competenze richieste dalle aziende per iniziare a fare questo lavoro. Oltre a fornirti conoscenze di programmazione, ti presentiamo alle nostre aziende partner che assumono in tutta Italia e ti aiutiamo a prepararti al meglio per i colloqui!</p>
+                    </div>
+                </div>   
+            </div>
+        </div>
     </section>
     
 @endsection
